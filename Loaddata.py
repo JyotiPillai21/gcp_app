e6cca70f4fca08b4029e2645ddf042f763f28692
JyotiPillai21 <jypillai@galaxe.com>
2023-03-14 17:37:58 +0530

    test commit

--- Loaddata.py
+++ Loaddata.py
@@ -1,83 +1,71 @@
-from __future__ import absolute_import
 import argparse
 import logging
 import re
 import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 import os
 
-class DataIngestion:
-    def parse_method(self, string_input):
-        values = re.split(",",
-                          re.sub('\r\n', '', re.sub(u'"', '', string_input)))
-        row = dict(
-            zip(('date', 'volume', 'open', 'close', 'high', 'low', 'adjclose'),
-                values))
-        return row
+# ParDo Class for Parallel processing by applying user defined transformations
 
+class convert_row(beam.DoFn):
+    def process(self,element):
+        try:
+            line = element.split(',')
+            tp =line[0] + ',' + line[1] + ',' + line[2] + ',' + line[3]
+            tp = tp.replace('"','')
+            tp = tp.split()
+            return tp
+        except:
+            logging.info('Some error occured')
 
-def run(argv=None):
-    """The main function which creates the pipeline and runs it."""
+# Entry run method for triggering pipeline
+    def run(self):
+        parser =argparse.ArgumentParser()
+        parser.add_argument('--input',
+                            dest='input',
+                            default='gs://gcp-demobucket',
+                            help='Input file to process.')
+        parser.add_argument('--output',
+                            dest='output',
+                            required=True,
+                            help='Output file to write results to.')
+        known_args, pipeline_args = parser.parse_known_args()
 
-    parser = argparse.ArgumentParser()
+        qry = ''' select Symbol, Sum(QuantityTraded) as QuantityTraded 
+                  from (
+                          select Symbol,
+                                 case when BuyorSell ='BUY' then cast(replace(QuantityTraded,',','') as int64)
+                                 else -cast(replace(QuantityTraded,',','') as int64) end as QuantityTraded
+                  from PCOLLECTION
+                        )
+                  group by 1'''
 
-    parser.add_argument(
-        '--input',
-        dest='input',
-        required=False,
-        help='Input file to read. This can be a local file or '
-             'a file in a Google Storage Bucket.',
-        default='gs://stocks-project-2/full_history/AADR.csv')
+        #Main Pipeline
 
-    # This defaults to the lake dataset in your BigQuery project. You'll have
-    # to create the lake dataset yourself using this command:
-    # bq mk lake
-    parser.add_argument('--output',
-                        dest='output',
-                        required=False,
-                        help='Output BQ table to write results to.',
-                        default='stocks_data.historical_prices')
+        with beam.pipeline(options = PipelineOptions(pipeline_args)) as p:
+            lines = p | 'read' >> ReadFromText(known_args.input,skip_header_lines=1)
+            counts = (
+                        lines
+                        | 'Formatted rows of strings' >> beam.ParDo(convert_row())
+                        | 'Convert as table rows' >> beam.map(lambda x:beam.Row(Symbol = str(x.split(',')[0]),BuyorSell = str(x.split(',')[1]), QuantityTraded = str(x.split(',')[2])))
+                        | 'Convert to Bigquery readable Dict' >> beam.Map(lambda row : row._asdict())
+            )
 
-    # Parse arguments from the command line.
-    known_args, pipeline_args = parser.parse_known_args(argv)
+        # Write to BigQuery
+        counts| 'Write to BigQuery' >> beam.io.Write(
+                                                    beam.io.WriteToBigQuery(
+                                                                                'batch_data',
+                                                                                dataset= 'dataflow_demo',
+                                                                                project='gcpproject-380209',
+                                                                                schema= 'Symbol:STRING,QuantityTraded:INTEGER',
+                                                                                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                                                                write_disposition= beam.io.BigQueryDisposition.WRITE_TRUNCATE
+                                                                           )
+                                                    )
 
-    # DataIngestion is a class we built in this script to hold the logic for
-    # transforming the file into a BigQuery table.
-    data_ingestion = DataIngestion()
-
-    # Initiate the pipeline using the pipeline arguments passed in from the
-    # command line. This includes information such as the project ID and
-    # where Dataflow should store temp files.
-    p = beam.Pipeline(options=PipelineOptions(pipeline_args))
-
-    (
-            p | 'Read from a File' >> beam.io.ReadFromText(known_args.input,
-                                                           skip_header_lines=1)
-
-            # This stage of the pipeline translates from a CSV file single row
-            # input as a string, to a dictionary object consumable by BigQuery.
-            # It refers to a function we have written. This function will
-            # be run in parallel on different workers using input from the
-            # previous stage of the pipeline.
-            | 'String To BigQuery Row' >>
-            beam.Map(lambda s: data_ingestion.parse_method(s))
-            | 'Write to BigQuery' >> beam.io.Write(
-        beam.io.BigQuerySink(
-            # The table name is a required argument for the BigQuery sink.
-            # In this case we use the value passed in from the command line.
-            known_args.output,
-            # Here we use the simplest way of defining a schema:
-            # fieldName:fieldType
-
-            schema='date:DATE,volume:INTEGER,open:FLOAT,close:FLOAT,high:FLOAT,low:FLOAT,adjclose:FLOAT',
-
-            # Creates the table in BigQuery if it does not yet exist.
-            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-            # Deletes all data in the BigQuery table before writing.
-            write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)))
-    p.run().wait_until_finish()
-
-
-if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
-    run()
+        # Trigger entry function here
+        if __name__ == '__main__':
+            logging.getLogger().setLevel(logging.INFO)
+            self.run()
